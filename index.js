35bdf67efa5c2c208d274bd15121f96f7135af5c
Viktoria Pruchkovskaya <pruchkovskayav@gmail.com>
2018-04-15 22:45:48 +0300

    second commit (work with Express)

--- index.js
+++ index.js
@@ -26,8 +26,20 @@ app.get('/get-goods/:type/:ordering', (req, res) => {
     if(type !== 'phones'){
         res.sendStatus(400);
     }
+    else{
+        res.send(JSON.stringify(`Type of requested goods ${type}`));
+    }
+});
 
-})
+app.get('/get-goods/', (req, res) => {
+    let good = req.query.good;
+    let ordering = req.query["price-order"];
+
+    if(!good || !ordering){
+        res.sendStatus(404);
+        }
+    res.send(JSON.stringify(`Type of requested goods ${good}, price-order ${ordering}`));
+});
 
 app.listen(PORT, () => {
     console.log(`Server is listening on ${PORT}`);
